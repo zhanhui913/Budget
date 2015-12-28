5b205bfddd9df6e8bc7cf180ca08533fb3d662bc
Zhan <zhanhui913@gmail.com>
2015-12-27 21:56:40 -0800

    Working on CircleView

--- app/src/main/java/com/zhan/budget/Activity/TransactionInfoActivity.java
+++ app/src/main/java/com/zhan/budget/Activity/TransactionInfoActivity.java
@@ -7,9 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.zhan.budget.Etc.Constants;
 import com.zhan.budget.R;
@@ -20,6 +25,10 @@ public class TransactionInfoActivity extends AppCompatActivity {
     private boolean isEditMode = false;
     private Activity instance;
     private Toolbar toolbar;
+    private Button button1,button2,button3,button4,button5,button6,button7,button8,button9,buttonDot,button0,buttonX;
+    private TextView transactionCostView;
+    private String priceString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +47,22 @@ public class TransactionInfoActivity extends AppCompatActivity {
      * Perform all initializations here.
      */
     private void init(){
+        button1 = (Button)findViewById(R.id.number1);
+        button2 = (Button)findViewById(R.id.number2);
+        button3 = (Button)findViewById(R.id.number3);
+        button4 = (Button)findViewById(R.id.number4);
+        button5 = (Button)findViewById(R.id.number5);
+        button6 = (Button)findViewById(R.id.number6);
+        button7 = (Button)findViewById(R.id.number7);
+        button8 = (Button)findViewById(R.id.number8);
+        button9 = (Button)findViewById(R.id.number9);
+        buttonDot = (Button)findViewById(R.id.numberDot);
+        button0 = (Button)findViewById(R.id.number0);
+        buttonX = (Button)findViewById(R.id.numberX);
 
+        transactionCostView = (TextView)findViewById(R.id.transactionCostText);
+
+        priceString = "";
 
         createToolbar();
         addListeners();
@@ -66,16 +90,174 @@ public class TransactionInfoActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //confirmLeave();
                 finish();
             }
         });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(2);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(3);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(4);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(5);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(6);
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(7);
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(8);
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(9);
+            }
+        });
+
+        buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDigitToTextView(0);
+            }
+        });
+
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeDigit();
+            }
+        });
+
+        //transactionCostView.addTextChangedListener(tw);
+
+
+    }
+
+    TextWatcher tw = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (!s.toString().matches("^\\$(\\d{1,3}(\\,\\d{3})*|(\\d+))(\\.\\d{2})?$")) {
+                String userInput = "" + s.toString().replaceAll("[^\\d]", "");
+                StringBuilder cashAmountBuilder = new StringBuilder(userInput);
+
+                while (cashAmountBuilder.length() > 3 && cashAmountBuilder.charAt(0) == '0') {
+                    cashAmountBuilder.deleteCharAt(0);
+                }
+                while (cashAmountBuilder.length() < 3) {
+                    cashAmountBuilder.insert(0, '0');
+                }
+                cashAmountBuilder.insert(cashAmountBuilder.length() - 2, '.');
+
+                transactionCostView.removeTextChangedListener(this);
+                transactionCostView.setText(cashAmountBuilder.toString());
+
+                transactionCostView.setTextKeepState("$" + cashAmountBuilder.toString());
+                Selection.setSelection(transactionCostView.getEditableText(), cashAmountBuilder.toString().length() + 1);
+
+                transactionCostView.addTextChangedListener(this);
+            }
+        }
+    };
+
+    private void addDigitToTextView(int digit){
+        //transactionCostView.setText(transactionCostView.getText() + "" +digit);
+        priceString += digit;
+        StringBuilder cashAmountBuilder = new StringBuilder(priceString);
+
+        while (cashAmountBuilder.length() > 3 && cashAmountBuilder.charAt(0) == '0') {
+            cashAmountBuilder.deleteCharAt(0);
+        }
+        while (cashAmountBuilder.length() < 3) {
+            cashAmountBuilder.insert(0, '0');
+        }
+
+        cashAmountBuilder.insert(cashAmountBuilder.length() - 2, '.');
+        transactionCostView.setText("$"+cashAmountBuilder.toString());
+
+    }
+
+    private void addDot(){
+
+    }
+
+    private void removeDigit(){
+        if (priceString != null && priceString.length() >= 1) {
+            priceString = priceString.substring(0, priceString.length() - 1);
+        }
+
+        StringBuilder cashAmountBuilder = new StringBuilder(priceString);
+
+        while (cashAmountBuilder.length() < 3) {
+            cashAmountBuilder.insert(0, '0');
+        }
+
+        cashAmountBuilder.insert(cashAmountBuilder.length() - 2, '.');
+        transactionCostView.setText("$"+cashAmountBuilder.toString());
 
     }
 
     @Override
     public void onBackPressed() {
-        //confirmLeave();
         finish();
     }
 
@@ -127,6 +309,8 @@ public class TransactionInfoActivity extends AppCompatActivity {
         setResult(RESULT_OK, intent);
         finish();
     }
+
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //
---
+++ app/src/main/java/com/zhan/budget/View/CircleView.java
@@ -0,0 +1,142 @@
+package com.zhan.budget.View;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.zhan.budget.R;
+
+/**
+ * Created by Zhan on 15-12-26.
+ */
+public class CircleView extends View {
+    //circle and text colors
+    private int circleCol, labelCol;
+
+    //label text
+    private String circleText;
+
+    //icon
+    private int circleIcon;
+
+
+    //paint for drawing custom view
+    private Paint circlePaint;
+
+    public CircleView(Context context, AttributeSet attrs){
+        super(context, attrs);
+
+        //paint object for drawing in onDraw
+        circlePaint = new Paint();
+
+        //get the attributes specified in attrs.xml using the name we included
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.LovelyView, 0, 0);
+
+        try {
+            //get the text and colors specified using the names in attrs.xml
+            circleText = a.getString(R.styleable.LovelyView_circleLabel);
+            circleCol = a.getInteger(R.styleable.LovelyView_circleColor, 0);//0 is default
+            circleIcon = a.getInteger(R.styleable.LovelyView_circleIcon, 0);//0 is default
+            labelCol = a.getInteger(R.styleable.LovelyView_labelColor, 0);
+        } finally {
+            a.recycle();
+        }
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //draw the View
+        //get half of the width and height as we are working with a circle
+        int viewWidthHalf = this.getMeasuredWidth()/2;
+        int viewHeightHalf = this.getMeasuredHeight()/2;
+
+        //get the radius as half of the width or height, whichever is smaller
+        //subtract ten so that it has some space around it
+        int radius = 0;
+        if(viewWidthHalf>viewHeightHalf) {
+            radius = viewHeightHalf - 10;
+        }else {
+            radius = viewWidthHalf - 10;
+        }
+
+        circlePaint.setStyle(Style.FILL);
+        circlePaint.setAntiAlias(true);
+
+        //set the paint color using the circle color specified
+        circlePaint.setColor(circleCol);
+
+        canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
+
+        //set the text color using the color specified
+        circlePaint.setColor(labelCol);
+
+        //set text properties
+        circlePaint.setTextAlign(Paint.Align.CENTER);
+        circlePaint.setTextSize(50);
+
+        //draw the text using the string attribute and chosen properties
+        canvas.drawText(circleText, viewWidthHalf, viewHeightHalf, circlePaint);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), circleIcon);
+
+        //canvas.drawBitmap(bitmap, 0, 0, circlePaint);
+    }
+
+
+    public int getCircleColor(){
+        return circleCol;
+    }
+
+    public int getLabelColor(){
+        return labelCol;
+    }
+
+    public String getLabelText(){
+        return circleText;
+    }
+
+    public int getCircleIcon(){
+        return circleIcon;
+    }
+
+    public void setCircleColor(int newColor){
+        //update the instance variable
+        circleCol=newColor;
+        //redraw the view
+        invalidate();
+        requestLayout();
+    }
+    public void setLabelColor(int newColor){
+        //update the instance variable
+        labelCol=newColor;
+        //redraw the view
+        invalidate();
+        requestLayout();
+    }
+
+    public void setLabelText(String newLabel){
+        //update the instance variable
+        circleText=newLabel;
+        //redraw the view
+        invalidate();
+        requestLayout();
+    }
+
+    public void setCircleIcon(int newIcon){
+        //update the instance variable
+        circleIcon=newIcon;
+        //redraw the view
+        invalidate();
+        requestLayout();
+    }
+}
