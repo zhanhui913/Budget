84676d8bec3f238ddcc61e7b69475e383a06b257
Zhan Yap <zyap@ualberta.ca>
2016-01-08 09:17:48 -0800

    Added icon with ability to set size and color

--- circularview/src/main/java/com/zhan/circularview/CircularView.java
+++ circularview/src/main/java/com/zhan/circularview/CircularView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
 
 /**
  * Created by Zhan on 16-01-07.
@@ -17,11 +19,10 @@ public class CircularView extends View {
     private int bg_color;
     private int stroke_width;
     private int stroke_color;
-    private Paint paintCircle;
-    private Paint paintBorder;
-
-    private int drawable;
-    private ImageView icon;
+    private int icon_size;
+    private int icon_color;
+    private Drawable drawable;
+    private Paint paint;
 
     public CircularView(Context context) {
         super(context);
@@ -46,26 +47,20 @@ public class CircularView extends View {
     private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes){
         this.context = context;
 
-        paintCircle = new Paint();
-        paintBorder = new Paint();
-
-        paintCircle.setAntiAlias(true);
-        paintBorder.setAntiAlias(true);
-
-
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CircularCellView, 0, 0);
         try{
             bg_color = a.getColor(R.styleable.CircularCellView_bg_color, getResources().getColor(android.R.color.holo_blue_light)); //default is holo_blue_light
             stroke_width = a.getInteger(R.styleable.CircularCellView_stroke_width, 0);//default is 0
             stroke_color = a.getColor(R.styleable.CircularCellView_stroke_color, getResources().getColor(android.R.color.holo_blue_light)); //default is holo_blue_light
-            drawable = a.getInteger(R.styleable.CircularCellView_cv_icon, 0);
+            drawable = a.getDrawable(R.styleable.CircularCellView_cv_icon);
+            icon_size = a.getInteger(R.styleable.CircularCellView_icon_size, 3);
+            icon_color = a.getColor(R.styleable.CircularCellView_icon_color, getResources().getColor(android.R.color.white)); //default is white
         }finally {
             a.recycle();
         }
 
-        if(drawable != 0){
-            icon.setBackgroundResource(drawable);
-        }
+        paint = new Paint();
+        paint.setAntiAlias(true);
     }
 
     @Override
@@ -92,6 +87,7 @@ public class CircularView extends View {
         }
 
         drawCircle(canvas, radius, viewWidthHalf, viewHeightHalf);
+        drawIcon(canvas);
     }
 
     private int getDesiredWidth() {
@@ -103,34 +99,91 @@ public class CircularView extends View {
     }
 
     private void drawCircle(Canvas canvas, int radius, int width, int height){
-        paintCircle.setStyle(Paint.Style.FILL);
-        paintCircle.setColor(bg_color);
-        canvas.drawCircle(width, height, radius, paintCircle);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(bg_color);
+        canvas.drawCircle(width, height, radius, paint);
 
-        paintBorder.setStyle(Paint.Style.STROKE);
-        paintBorder.setColor(stroke_color);
-        paintBorder.setStrokeWidth(stroke_width);
-        canvas.drawCircle(width, height, radius, paintBorder);
+        if(stroke_width > 0) {
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(stroke_color);
+            float saveStrokeWidth = paint.getStrokeWidth();
+            paint.setStrokeWidth(stroke_width);
+            canvas.drawCircle(width, height, radius - (stroke_width / 2), paint);
+            paint.setStrokeWidth(saveStrokeWidth);
+        }
     }
 
-    /**
-     * Sets color to shape.
-     *
-     * @param color a color integer associated with a particular resource id
-     */
-    public void setShapeColor(int color) {
-        this.bg_color = color;
-        invalidate();
+    private void drawIcon(Canvas canvas){
+        if(drawable != null){
+            Rect bounds = canvas.getClipBounds();
+
+            int multiplier = 5;
+
+            bounds.left += (icon_size * multiplier);
+            bounds.right -= (icon_size * multiplier);
+            bounds.top += (icon_size * multiplier);
+            bounds.bottom -= (icon_size * multiplier);
+
+            drawable.setBounds(bounds);
+            drawable.mutate().setColorFilter(icon_color, PorterDuff.Mode.SRC_IN);
+            drawable.draw(canvas);
+        }
     }
 
-    public int getBgColor(){
-        return this.bg_color;
+
+    public Paint getPaint() {
+        return paint;
     }
 
-    public Paint getShapePaint() {
-        return paintCircle;
+    public int getBg_color() {
+        return bg_color;
     }
 
+    public void setBg_color(int bg_color) {
+        this.bg_color = bg_color;
+    }
 
+    public int getStroke_width() {
+        return stroke_width;
+    }
 
+    public void setStroke_width(int stroke_width) {
+        this.stroke_width = stroke_width;
+    }
+
+    public int getStroke_color() {
+        return stroke_color;
+    }
+
+    public void setStroke_color(int stroke_color) {
+        this.stroke_color = stroke_color;
+    }
+
+    public int getIcon_size() {
+        return icon_size;
+    }
+
+    public void setIcon_size(int icon_size) {
+        this.icon_size = icon_size;
+    }
+
+    public int getIcon_color() {
+        return icon_color;
+    }
+
+    public void setIcon_color(int icon_color) {
+        this.icon_color = icon_color;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public void setPaint(Paint paint) {
+        this.paint = paint;
+    }
 }
