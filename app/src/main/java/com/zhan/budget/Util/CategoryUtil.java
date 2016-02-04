593913aaf1a9566015ff6bf10c52819072bfad82
Zhan Yap <zyap@ualberta.ca>
2016-02-04 13:42:37 -0800

    Rename all icons that will be used for categories

--- app/src/main/java/com/zhan/budget/Util/CategoryUtil.java
+++ app/src/main/java/com/zhan/budget/Util/CategoryUtil.java
@@ -22,76 +22,76 @@ public final class CategoryUtil {
 
     public static Drawable getIconDrawable(Context context, int iconId){
         if(iconId == 0){
-            return ContextCompat.getDrawable(context, R.drawable.food);
+            return ContextCompat.getDrawable(context, R.drawable.c_food);
         }else if(iconId == 1){
-            return ContextCompat.getDrawable(context, R.drawable.food);
+            return ContextCompat.getDrawable(context, R.drawable.c_food);
         }else if(iconId == 2){
-            return ContextCompat.getDrawable(context, R.drawable.food);
+            return ContextCompat.getDrawable(context, R.drawable.c_food);
         }else if(iconId == 3){
-            return ContextCompat.getDrawable(context, R.drawable.food);
+            return ContextCompat.getDrawable(context, R.drawable.c_food);
         }else if(iconId == 4){
-            return ContextCompat.getDrawable(context, R.drawable.cafe);
+            return ContextCompat.getDrawable(context, R.drawable.c_cafe);
         }else if(iconId == 5){
-            return ContextCompat.getDrawable(context, R.drawable.house);
+            return ContextCompat.getDrawable(context, R.drawable.c_house);
         }else if(iconId == 6){
-            return ContextCompat.getDrawable(context, R.drawable.airplane);
+            return ContextCompat.getDrawable(context, R.drawable.c_airplane);
         }else if(iconId == 7){
-            return ContextCompat.getDrawable(context, R.drawable.car);
+            return ContextCompat.getDrawable(context, R.drawable.c_car);
         }else if(iconId == 8){
-            return ContextCompat.getDrawable(context, R.drawable.shirt);
+            return ContextCompat.getDrawable(context, R.drawable.c_shirt);
         }else if(iconId == 9){
-            return ContextCompat.getDrawable(context, R.drawable.etc);
+            return ContextCompat.getDrawable(context, R.drawable.c_etc);
         }else if(iconId == 10){
-            return ContextCompat.getDrawable(context, R.drawable.utilities);
+            return ContextCompat.getDrawable(context, R.drawable.c_utilities);
         }else if(iconId == 11){
-            return ContextCompat.getDrawable(context, R.drawable.bill);
+            return ContextCompat.getDrawable(context, R.drawable.c_bill);
         }else{
-            return ContextCompat.getDrawable(context, R.drawable.groceries);
+            return ContextCompat.getDrawable(context, R.drawable.c_groceries);
         }
     }
 
     public static int getIconResourceId(int iconId){
         if(iconId == 0){
-            return R.drawable.food;
+            return R.drawable.c_food;
         }else if(iconId == 1){
-            return R.drawable.food;
+            return R.drawable.c_food;
         }else if(iconId == 2){
-            return R.drawable.food;
+            return R.drawable.c_food;
         }else if(iconId == 3){
-            return R.drawable.food;
+            return R.drawable.c_food;
         }else if(iconId == 4){
-            return R.drawable.cafe;
+            return R.drawable.c_cafe;
         }else if(iconId == 5){
-            return R.drawable.house;
+            return R.drawable.c_house;
         }else if(iconId == 6){
-            return R.drawable.airplane;
+            return R.drawable.c_airplane;
         }else if(iconId == 7){
-            return R.drawable.car;
+            return R.drawable.c_car;
         }else if(iconId == 8){
-            return R.drawable.shirt;
+            return R.drawable.c_shirt;
         }else if(iconId == 9){
-            return R.drawable.etc;
+            return R.drawable.c_etc;
         }else if(iconId == 10){
-            return R.drawable.utilities;
+            return R.drawable.c_utilities;
         }else if(iconId == 11){
-            return R.drawable.bill;
+            return R.drawable.c_bill;
         }else{
-            return R.drawable.groceries;
+            return R.drawable.c_groceries;
         }
     }
 
     public static List<Integer> getListOfUniqueIcon(){
         List<Integer> iconList = new ArrayList<>();
-        iconList.add(R.drawable.food);
-        iconList.add(R.drawable.cafe);
-        iconList.add(R.drawable.house);
-        iconList.add(R.drawable.airplane);
-        iconList.add(R.drawable.car);
-        iconList.add(R.drawable.shirt);
-        iconList.add(R.drawable.etc);
-        iconList.add(R.drawable.utilities);
-        iconList.add(R.drawable.bill);
-        iconList.add(R.drawable.groceries);
+        iconList.add(R.drawable.c_food);
+        iconList.add(R.drawable.c_cafe);
+        iconList.add(R.drawable.c_house);
+        iconList.add(R.drawable.c_airplane);
+        iconList.add(R.drawable.c_car);
+        iconList.add(R.drawable.c_shirt);
+        iconList.add(R.drawable.c_etc);
+        iconList.add(R.drawable.c_utilities);
+        iconList.add(R.drawable.c_bill);
+        iconList.add(R.drawable.c_groceries);
 
         return iconList;
     }
