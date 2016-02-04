987b9880ccb22db0d8e325a9ceb476d8819997a2
Zhan Yap <zyap@ualberta.ca>
2016-02-04 14:52:21 -0800

    Able to get the name of the drawable from its integer ID

--- app/src/main/java/com/zhan/budget/Util/CategoryUtil.java
+++ app/src/main/java/com/zhan/budget/Util/CategoryUtil.java
@@ -89,7 +89,7 @@ public final class CategoryUtil {
         for(int i = 0; i < icons.length(); i++){
             // get resource ID by index
             int s = icons.getResourceId(i, 0);
-            Log.d("ZHAN", i+"->"+s);
+            Log.d("ZHAN", i+"->"+s+", name :"+context.getResources().getResourceName(s)+", entry name:"+context.getResources().getResourceEntryName(s));
             iconList.add(s);
         }
 
