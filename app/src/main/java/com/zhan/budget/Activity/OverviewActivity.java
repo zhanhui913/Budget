d72bd36cf23e20a2ebda46e7b8b7694a0ebce3dc
zhanhui913 <zhanhui913@gmail.com>
2016-05-12 12:34:21 -0700

    Fix bug where pie chart isnt shown in overviewActivity

--- app/src/main/java/com/zhan/budget/Activity/OverviewActivity.java
+++ app/src/main/java/com/zhan/budget/Activity/OverviewActivity.java
@@ -308,6 +308,7 @@ public class OverviewActivity extends BaseActivity implements
                 barChartFragment = BarChartFragment.newInstance(categoryList);
                 percentChartFragment = PercentChartFragment.newInstance(categoryList);
                 pieChartFragment = PieChartFragment.newInstance(categoryList);
+                pieChartFragment.setData(categoryList);
                 getSupportFragmentManager().beginTransaction().add(R.id.chartContentFrame, barChartFragment).commit();
 
             }
