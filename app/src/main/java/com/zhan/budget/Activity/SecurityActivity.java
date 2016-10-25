efaa71b428427367e36557ee768ade9a6102826f
Zhan Yap <zyap@ualberta.ca>
2016-10-24 17:58:10 -0700

    #85; in process of handling multiple  fragments of security

---
+++ app/src/main/java/com/zhan/budget/Activity/SecurityActivity.java
@@ -0,0 +1,25 @@
+package com.zhan.budget.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.zhan.budget.R;
+
+public class SecurityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/zhan/budget/Fragment/Security/PinFragment.java
@@ -0,0 +1,29 @@
+package com.zhan.budget.Fragment.Security;
+
+import android.support.v4.app.Fragment;
+
+import com.zhan.budget.Fragment.BaseFragment;
+import com.zhan.budget.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PinFragment extends BaseFragment {
+
+    public PinFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_pin;
+    }
+
+    @Override
+    protected void init(){
+
+    }
+
+
+
+}
