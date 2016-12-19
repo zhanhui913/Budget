0e80eaec354bc54e667128c718305377e0d19f4a
zhanhui913 <zhanhui913@gmail.com>
2016-12-18 20:09:20 -0800

    #139; in process of adding functional search field in currency list

--- app/src/main/java/com/zhan/budget/Activity/SelectCurrencyActivity.java
+++ app/src/main/java/com/zhan/budget/Activity/SelectCurrencyActivity.java
@@ -1,13 +1,19 @@
 package com.zhan.budget.Activity;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -123,9 +129,9 @@ public class SelectCurrencyActivity extends BaseRealmActivity implements
 
         if(getSupportActionBar() != null){
             if(returnDefaultCurrency){
-                getSupportActionBar().setTitle("Select default currency");
+                getSupportActionBar().setTitle("Set currency");
             }else{
-                getSupportActionBar().setTitle("Select currency");
+                getSupportActionBar().setTitle("Set currency");
             }
         }
     }
@@ -287,6 +293,43 @@ public class SelectCurrencyActivity extends BaseRealmActivity implements
             }
         });
         massExchangeRate.execute();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Menu
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.searchBtn).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.formSaveBtn) {
+
+
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
