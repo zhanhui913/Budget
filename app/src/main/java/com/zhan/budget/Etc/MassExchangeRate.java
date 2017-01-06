b881126c3217f2a96053369e14b1883618e9eff1
Zhan Yap <zyap@ualberta.ca>
2017-01-06 13:21:09 -0800

    #148; dialog now has same style across all api

--- app/src/main/java/com/zhan/budget/Etc/MassExchangeRate.java
+++ app/src/main/java/com/zhan/budget/Etc/MassExchangeRate.java
@@ -1,6 +1,6 @@
 package com.zhan.budget.Etc;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
