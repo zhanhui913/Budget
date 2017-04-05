853c19ae96d0100602e4a6b713ec824941f8f157
Zhan Yap <zyap@ualberta.ca>
2017-04-05 14:44:02 -0700

    #169; added date to privacy policy

--- app/src/main/java/com/zhan/budget/Activity/Settings/PrivacyPolicyActivity.java
+++ app/src/main/java/com/zhan/budget/Activity/Settings/PrivacyPolicyActivity.java
@@ -2,11 +2,18 @@ package com.zhan.budget.Activity.Settings;
 
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.zhan.budget.Activity.BaseActivity;
 import com.zhan.budget.R;
+import com.zhan.budget.Util.DateUtil;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class PrivacyPolicyActivity extends BaseActivity {
+
+    Date privacyDate;
 
     @Override
     protected int getActivityLayout(){
@@ -36,5 +43,12 @@ public class PrivacyPolicyActivity extends BaseActivity {
                 finish();
             }
         });
+
+        //April 4, 2017
+        //Gregorian uses month starts at 0 for January.
+        Date lastUpdatedDate = new GregorianCalendar(2017, 3, 4).getTime();
+
+        TextView privacyDate = (TextView)findViewById(R.id.privacyDate);
+        privacyDate.setText(String.format(getString(R.string.privacy_last_updated_date), DateUtil.convertDateToStringFormat5(this, lastUpdatedDate)));
     }
 }
