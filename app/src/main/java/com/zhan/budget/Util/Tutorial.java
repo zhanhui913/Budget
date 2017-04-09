a7a6cd8762e89820f7ffb4882a3c25a087ccf8c3
zhanhui913 <zhanhui913@gmail.com>
2017-04-09 16:54:26 -0700

    #126; update string for tutorial

--- app/src/main/java/com/zhan/budget/Util/Tutorial.java
+++ app/src/main/java/com/zhan/budget/Util/Tutorial.java
@@ -18,40 +18,40 @@ public final class Tutorial {
 
     public static ArrayList<TutorialItem> getTutorialPages(Context context){
         TutorialItem page1 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_1),
-                context.getResources().getString(R.string.tutorial_description_1),
+                context.getResources().getString(R.string.tutorial_title_location),
+                context.getResources().getString(R.string.tutorial_description_location),
                 R.color.colorPrimary,
-                R.drawable.t_account);
+                R.drawable.t_location);
 
         TutorialItem page2 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_2),
-                context.getResources().getString(R.string.tutorial_description_2),
+                context.getResources().getString(R.string.tutorial_title_theme),
+                context.getResources().getString(R.string.tutorial_description_theme),
                 R.color.colorPrimary,
                 R.drawable.t_theme);
 
         TutorialItem page3 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_3),
-                context.getResources().getString(R.string.tutorial_description_3),
+                context.getResources().getString(R.string.tutorial_title_account),
+                context.getResources().getString(R.string.tutorial_description_account),
+                R.color.colorPrimary,
+                R.drawable.t_account);
+
+        TutorialItem page4 = new TutorialItem(
+                context.getResources().getString(R.string.tutorial_title_calculator),
+                context.getResources().getString(R.string.tutorial_description_calculator),
                 R.color.colorPrimary,
                 R.drawable.t_calculator);
 
-        TutorialItem page4 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_4),
-                context.getResources().getString(R.string.tutorial_description_4),
+        TutorialItem page5 = new TutorialItem(
+                context.getResources().getString(R.string.tutorial_title_budget),
+                context.getResources().getString(R.string.tutorial_description_budget),
                 R.color.colorPrimary,
                 R.drawable.t_balance);
 
-        TutorialItem page5 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_5),
-                context.getResources().getString(R.string.tutorial_description_5),
+        TutorialItem page6 = new TutorialItem(
+                context.getResources().getString(R.string.tutorial_title_chart),
+                context.getResources().getString(R.string.tutorial_description_chart),
                 R.color.colorPrimary,
                 R.drawable.t_pie);
-
-        TutorialItem page6 = new TutorialItem(
-                context.getResources().getString(R.string.tutorial_title_6),
-                context.getResources().getString(R.string.tutorial_description_6),
-                R.color.colorPrimary,
-                R.drawable.t_location);
 
         ArrayList<TutorialItem> tourItems = new ArrayList<>();
         tourItems.add(page1);
